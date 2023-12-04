002d24ad7fa439be308eddf241319f5e3f82f455
infogulch <infogulch@gmail.com>
2023-12-04 00:49:17 -0600

    day2 cleanup

--- 2023/day2.pl
+++ 2023/day2.pl
@@ -1,9 +1,12 @@
-% :- module(day2, []).
+:- module(day2, [
+    ws//0,
+    numeric_chars//1,
+    num//1,
+    list_max/3
+]).
 
 :- use_module(library(dcgs)).
 :- use_module(library(lists)).
-
-:- use_module(library(debug)).
 
 :- use_module(input).
 :- use_module(day1).
@@ -13,14 +16,14 @@ list_max([A|As],[B|Bs],[M|Ms]) :- M is max(A,B),list_max(As,Bs,Ms).
 
 ws --> ""|" ",ws.
 
-numeric([C|Cs]) --> [C],{char_type(C,numeric)},numeric(Cs).
-numeric("") --> "".
-num(N) --> numeric(Cs),{Cs=[_|_],number_chars(N,Cs)}.
+numeric_chars([C|Cs]) --> [C],{char_type(C,numeric)},numeric(Cs).
+numeric_chars("") --> "".
+num(N) --> numeric_chars(Cs),{Cs=[_|_],number_chars(N,Cs)}.
 
 game(Id,Rs) --> "Game ",num(Id),":",rounds(Rs).
 
 rounds([C]) --> ws,round(C).
-rounds([C0|C]) --> ws,round(C0),ws,";",rounds(C).
+rounds([C|Cs]) --> ws,round(C),ws,";",rounds(Cs).
 
 round(C) --> color(C).
 round(C) --> color(C0),",",round(C1),{list_max(C0,C1,C)}.
@@ -42,11 +45,9 @@ part1(Sum) :-
     maplist(game_max_id,Gs,Ids),
     sum_list(Ids,Sum).
 
-game_max_id(Id-Rounds,Id0) :-
-    transpose(Rounds,[R,G,B]),
-    list_max(R,MaxR),
-    list_max(G,MaxG),
-    list_max(B,MaxB),
+game_max_id(Id-RoundColors,Id0) :-
+    transpose(RoundColors,ColorRounds),
+    maplist(list_max, ColorRounds, [MaxR,MaxG,MaxB]),
     (MaxR =< 12,MaxG =< 13,MaxB =< 14 -> Id0 = Id; Id0 = 0).
 
 
@@ -56,10 +57,8 @@ part2(Sum) :-
     sum_list(Products,Sum).
 
 game_max_product(_-Rounds,Product) :-
-    transpose(Rounds,[R,G,B]),
-    list_max(R,MaxR),
-    list_max(G,MaxG),
-    list_max(B,MaxB),
+    transpose(RoundColors,ColorRounds),
+    maplist(list_max, ColorRounds, [MaxR,MaxG,MaxB]),
     Product is MaxR*MaxG*MaxB.
 
 run_infogulch :- part1(3035),part2(66027).
