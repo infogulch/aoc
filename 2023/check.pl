f29f3baf6b06316d649a7264869c16e25cb2dcce
infogulch <infogulch@gmail.com>
2023-12-05 20:58:16 -0600

    Fixup check and input modules

--- 2023/check.pl
+++ 2023/check.pl
@@ -1,12 +1,19 @@
+:- use_module(library(crypto)).
 
+:- use_module(input).
 :- use_module(day1).
 :- use_module(day2).
 % :- use_module(day3).
 :- use_module(day4).
 
-run :-
+check("45a6bcaa93cb837edbd0a55d87bebcb17052759dadf995357bd173f58bb763e6a13094b67d93b4ad36978e2ae8357a2c") :-
     day1:part1(54632),day1:part2(54019),
     day2:part1(3035),day2:part2(66027),
     day4:part1(21138),day4:part2(7185540).
+
+run :-
+    cookie(Cookie),
+    crypto_data_hash(Cookie,Hash,[algorithm(sha384)]),
+    check(Hash).
 
 :- initialization(run).
--- 2023/input.pl
+++ 2023/input.pl
@@ -1,4 +1,4 @@
-:- module(input, [input/2]).
+:- module(input, [input/2,cookie/1]).
 
 :- use_module(library(http/http_open)).
 :- use_module(library(si)).
@@ -8,13 +8,15 @@
 :- use_module(library(charsio)).
 :- use_module(library(files)).
 
+cookie(Cookie) :- phrase_from_file(seq(Cookie), "cookie.txt").
+
 input(N, Input) :-
     integer_si(N),
     (directory_exists("inputs") -> true ; make_directory("inputs")),
     phrase(format_("inputs/~w.txt", [N]), CachedInputFile),
     (file_exists(CachedInputFile) -> phrase_from_file(seq(Input), CachedInputFile)
     ; phrase(format_("https://adventofcode.com/2023/day/~w/input", [N]), Url),
-    phrase_from_file(seq(Cookie), "cookie.txt"),
+    cookie(Cookie),
     http_open(Url, Response, [request_headers(['Cookie'(Cookie)])]),
     get_n_chars(Response,_Size,Input),
     phrase_to_file(seq(Input),CachedInputFile)
